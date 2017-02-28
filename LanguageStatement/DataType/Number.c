544cd2f89bfce5300fe4573e59359d235ee7c07a
liuyangspace <2260597513@qq.com>
2017-02-28 17:05:55 +0800

    Number

---
+++ LanguageStatement/DataType/Number.c
@@ -0,0 +1,17 @@
+/*
+数字类型
+    整形
+        声明：[signed/unsigned] [short/long] int
+        0 开头表示八进制，0x 或 0X 开头表示十六进制，其他为十进制
+        以字母 u 或 U 结尾表示无符号数，以字母 l 或 L 结尾表示长整形
+    浮点型
+*/
+
+int number1 = 1234567890;
+int number2 = 01234567;
+int number3 = 0x1abcdef;
+
+int main()
+{
+
+}
